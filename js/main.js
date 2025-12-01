ecebf7b0fb7b08b95a4aa5bc21b332c69447f188
Simon-DaSilva <simon.dasilva.to@gmail.com>
2025-12-01 17:00:30 -0500

    changed js files

--- js/main.js
+++ js/main.js
@@ -14,31 +14,27 @@
   //This information needs to be removed then pulled with an AJAX Call using the Fetch API
   //this is the api url https://swiftpixel.com/earbud/api/materials"
 
-  const materialListData = [
-    {
-      heading: "Precision-Crafted Polymers",
-      description: "Our earbuds are meticulously molded from high-quality plastics, ensuring a blend of elegance, comfort, and resilience that's second to none."
-    },
-    {
-      heading: "Luxurious Silicone Harmony",
-      description: "Our uniquely engineered ear tips are cocooned in plush silicone, delivering an opulent embrace for your ears, ensuring an unrivaled fit and exquisite audio experience."
-    },
-    {
-      heading: "Rubberized Cables",
-      description: "Experience the unparalleled freedom of movement with our flexible rubber cables that promise durability without compromise."
-    },
-    {
-      heading: "Enhanced Comfort Sensors",
-      description: "A touch of magic in the form of built-in microphones and sensors empowers your earbuds to obey your every command, making your audio journey seamless and enchanting."
-    },
-    {
-      heading: "Artistic Mesh Guard",
-      description: "Shielded by artful mesh screens, our speakers remain untarnished, keeping your listening experience pristine."
-    }
-  ];
+
 
   //functions
-  function loadInfoBoxes() {
+ 
+
+   
+    //add loader to html
+    function materialListData() {
+      fetch ("https://swiftpixel.com/earbud/api/materials")
+      .then(response => response.json())
+      .then(materials => {
+        console.log(materials);
+      })
+      .catch(error);
+
+    }
+
+    materialListData();
+    //make AJAX call here
+
+     function loadInfoBoxes() {
     fetch("https://swiftpixel.com/earbud/api/infoboxes")
     .then(response => response.json())
     .then(infoBoxes => {
@@ -47,12 +43,9 @@
     .catch(error);
     }
 
-   loadInfoBoxes();
+    
 
-    //add loader to html
-
-    //make AJAX call here
-
+    function loadInfoBoxes() {
     infoBoxes.forEach((infoBox, index) => {
       let selected = document.querySelector(`#hotspot-${index + 1}`);
 
@@ -65,28 +58,32 @@
       selected.appendChild(titleElement);
       selected.appendChild(textElement);
     });
+    };
+
+    loadInfoBoxes();
   
  
 
   function loadMaterialInfo() {
-      materialListData.forEach((material) => {
+      materialListData.forEach((materials) => {
         //clone template li with h3 and p inside
         const clone = materialTemplate.content.cloneNode(true);
 
         //populate clone template
-        const materialHeading = clone.querySelector(".material-heading");
-        materialHeading.textContent = material.heading;
+        const materialHeading = clone.querySelector(".materials-heading");
+        materialHeading.textContent = materials.heading;
 
-        const materialDescription = clone.querySelector(".material-description");
-        materialDescription.textContent = material.description;
+        const materialDescription = clone.querySelector(".materials-description");
+        materialDescription.textContent = materials.description;
 
         //hide loader
         //append the populated template to li
         materialList.appendChild(clone);
       });
+      
     };
     loadMaterialInfo();
-
+    
 
 
   function showInfo() {
@@ -94,10 +91,14 @@
     gsap.to(selected, 1, { autoAlpha: 1 });
   }
 
+  showInfo();
+
   function hideInfo() {
     let selected = document.querySelector(`#${this.slot}`);
     gsap.to(selected, 1, { autoAlpha: 0 });
   }
+
+  loadInfo();
 
   //Event listeners
 
